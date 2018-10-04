30ffc3f3b3b922fd5a4daa3a5eb8264e359325b6
Dmytro Semenov <dsemenov@gilt.com>
2018-10-04 16:42:35 -0400

    handle exceptions inside effectful function

--- src/main/scala/fs2/aws/internal/Internal.scala
+++ src/main/scala/fs2/aws/internal/Internal.scala
@@ -6,13 +6,16 @@ import cats.effect.Effect
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.amazonaws.services.s3.model._
 
+import scala.util.control.Exception
+
 object Internal {
+
   private[aws] trait S3Client[F[_]] {
     private val client = AmazonS3ClientBuilder.defaultClient
 
     def getObjectContent(getObjectRequest: GetObjectRequest)(
-        implicit F: Effect[F]): F[S3ObjectInputStream] =
-      F.delay(client.getObject(getObjectRequest).getObjectContent)
+        implicit F: Effect[F]): F[Either[Throwable, S3ObjectInputStream]] =
+      F.delay(Exception.nonFatalCatch either client.getObject(getObjectRequest).getObjectContent)
 
     def initiateMultipartUpload(initiateMultipartUploadRequest: InitiateMultipartUploadRequest)(
         implicit F: Effect[F]): F[InitiateMultipartUploadResult] =
@@ -29,4 +32,5 @@ object Internal {
   }
 
   private[aws] case class MultiPartUploadInfo(uploadId: String, partETags: List[PartETag])
+
 }
--- src/main/scala/fs2/aws/s3/s3.scala
+++ src/main/scala/fs2/aws/s3/s3.scala
@@ -18,32 +18,25 @@ package object s3 {
       s3Client: S3Client[F] = new S3Client[F] {})(implicit F: Effect[F]): fs2.Stream[F, Byte] = {
     def go(offset: Int)(implicit F: Effect[F]): fs2.Pull[F, Byte, Unit] =
       fs2.Pull
-        .acquire[F, Option[S3ObjectInputStream]]({
-          try s3Client
-            .getObjectContent(
-              new GetObjectRequest(bucket, key).withRange(offset, offset + chunkSize))
-            .map(Some.apply)
-          catch {
-            case _: AmazonS3Exception => F.pure(None)
-          }
-        })({
-          case Some(s) => F.delay(s.abort())
-          case None    => F.delay(() => ())
-        })
-        .flatMap({
-          case Some(s) =>
+        .acquire[F, Either[Throwable, S3ObjectInputStream]](s3Client.getObjectContent(
+          new GetObjectRequest(bucket, key).withRange(offset, offset + chunkSize))) {
+          case Left(_)  => F.delay(() => ())
+          case Right(s) => F.delay(s.abort())
+        }
+        .flatMap {
+          case Right(s) =>
             fs2.Pull.eval(F.delay {
               val is: InputStream = s
               val buf             = new Array[Byte](chunkSize)
-              val len = is.read(buf)
+              val len             = is.read(buf)
               Some(Chunk.bytes(buf, 0, len))
             })
-          case None => fs2.Pull.eval(F.delay(None))
-        })
-        .flatMap({
+          case Left(_) => fs2.Pull.eval(F.delay(None))
+        }
+        .flatMap {
           case Some(o) => fs2.Pull.outputChunk(o) >> go(offset + o.size)
           case None    => fs2.Pull.done
-        })
+        }
 
     go(0).stream
 
