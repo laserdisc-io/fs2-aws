c74a30c8286a6dff25b87752722d82145cccd768
semenodm <sdo.semenov@gmail.com>
2018-10-11 15:26:35 -0400

    Exceptions in effectful function (#4)
    * handle exceptions inside effectful function
    
    * apply code review

--- src/test/scala/fs2/aws/utils/package.scala
+++ src/test/scala/fs2/aws/utils/package.scala
@@ -14,29 +14,30 @@ import scala.io.Source
 package object utils {
 
   val s3TestClient: S3Client[IO] = new S3Client[IO] {
-    override def getObjectContent(getObjectRequest: GetObjectRequest)(implicit e: Effect[IO]) : IO[S3ObjectInputStream] = getObjectRequest match {
+    override def getObjectContent(getObjectRequest: GetObjectRequest)(implicit e: Effect[IO]): IO[Either[Throwable, S3ObjectInputStream]] = getObjectRequest match {
       case goe: GetObjectRequest => {
-        val is : InputStream = {
+        IO[Either[Throwable, ByteArrayInputStream]] {
           val fileContent: Array[Byte] =
             try {
               Source.fromResource(goe.getKey).mkString.getBytes
             }
             catch {
-              case _ : FileNotFoundException => throw new AmazonS3Exception("File not found")
+              case _: FileNotFoundException => throw new AmazonS3Exception("File not found")
               case e: Throwable => throw e
             }
           goe.getRange match {
             case Array(x, y) =>
-
-              if (x >= fileContent.length) throw new AmazonS3Exception("Invalid range")
-              else if (y > fileContent.length) new ByteArrayInputStream(fileContent.slice(x.toInt, fileContent.length))
-              else new ByteArrayInputStream(fileContent.slice(x.toInt, y.toInt))
+              if (x >= fileContent.length) Left(new AmazonS3Exception("Invalid range"))
+              else if (y > fileContent.length) Right(new ByteArrayInputStream(fileContent.slice(x.toInt, fileContent.length)))
+              else Right(new ByteArrayInputStream(fileContent.slice(x.toInt, y.toInt)))
           }
-        }
 
-        IO {
-          Thread.sleep(500)  // simulate a call to S3
-          new S3ObjectInputStream(is, new HttpRequestBase { def getMethod = "" })
+        } map { case Left(e) => Left(e)
+        case Right(is) =>
+          Thread.sleep(500) // simulate a call to S3
+          Right(new S3ObjectInputStream(is, new HttpRequestBase {
+            def getMethod = ""
+          }))
         }
       }
       case _ => throw new SdkClientException("Invalid GetObjectRequest")
