a3881e8a172d12f8deb5d6622807679daaee98f6
Christopher Poenaru <kiambogo@gmail.com>
2018-10-26 13:32:38 -0700

    Update groupBy description for clarity

--- src/main/scala/fs2/aws/internal/Internal.scala
+++ src/main/scala/fs2/aws/internal/Internal.scala
@@ -53,30 +53,37 @@ object Internal {
   /** Helper flow to group elements of a stream into K substreams.
     * Grows with the number of distinct 'K' selectors
     *
+    * Start with an empty Map of keys to queues
+    * On element received, invoke the selector function to yield the key denoting which queue this element belongs to
+    * If we already have an existing queue for that respective key, append the element lifted in an Option to the queue
+    * If a queue for that key does not exist, create a new queue, append it to the queue mapping, and then enqueue the element lifted in an Option
+    * For each queue, drain the queue yielding a stream of elements
+    * After the stream has been emptied, enqueue a single None to the queue so that the stream halts
+    *
     *  @tparam F effect type of the fs2 stream
     *  @param selector partitioning function based on the element
     *  @return a FS2 pipe producing a new sub-stream of elements grouped by the selector
     */
   def groupBy[F[_], A, K](selector: A => F[K])(
       implicit F: Concurrent[F]): Pipe[F, A, (K, Stream[F, A])] = { in =>
-    Stream.eval(Ref.of[F, Map[K, Queue[F, Option[A]]]](Map.empty)).flatMap { st =>
+    Stream.eval(Ref.of[F, Map[K, Queue[F, Option[A]]]](Map.empty)).flatMap { queueMap =>
       val cleanup = {
         import alleycats.std.all._
-        st.get.flatMap(_.traverse_(_.enqueue1(None)))
+        queueMap.get.flatMap(_.traverse_(_.enqueue1(None)))
       }
 
       (in ++ Stream.eval_(cleanup))
-        .evalMap { el =>
-          (selector(el), st.get).mapN { (key, queues) =>
+        .evalMap { elem =>
+          (selector(elem), queueMap.get).mapN { (key, queues) =>
             queues
               .get(key)
               .fold {
                 for {
                   newQ <- Queue.unbounded[F, Option[A]] // Create a new queue
-                  _    <- st.modify(x => (x + (key -> newQ), x)) // Update the ref of queues
-                  _    <- newQ.enqueue1(el.some)
+                  _    <- queueMap.modify(queues => (queues + (key -> newQ), queues)) // Update the ref of queues
+                  _    <- newQ.enqueue1(elem.some) // Enqueue the element lifted into an Option to the new queue
                 } yield (key -> newQ.dequeue.unNoneTerminate).some
-              }(_.enqueue1(el.some) as None)
+              }(_.enqueue1(elem.some) as None)
           }.flatten
         }
         .unNone
