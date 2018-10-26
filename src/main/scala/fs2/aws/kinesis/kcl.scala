936346d7d99544bb9424b5c6eb498267cf3e6c6b
Christopher Poenaru <kiambogo@gmail.com>
2018-10-25 19:26:30 -0700

    Add Worker shutdown on stream failure

--- src/main/scala/fs2/aws/kinesis/kcl.scala
+++ src/main/scala/fs2/aws/kinesis/kcl.scala
@@ -7,12 +7,13 @@ import fs2.concurrent.Queue
 import cats.effect.{ConcurrentEffect, IO, Timer}
 import cats.implicits._
 
-import com.amazonaws.services.kinesis.clientlibrary.interfaces.v2.IRecordProcessorFactory
-import com.amazonaws.services.kinesis.clientlibrary.lib.worker.Worker
 import com.amazonaws.services.kinesis.model.Record
+import com.amazonaws.services.kinesis.clientlibrary.lib.worker.Worker
+import com.amazonaws.services.kinesis.clientlibrary.interfaces.v2.IRecordProcessorFactory
 
 package object kcl {
   /** Intialize a worker and start streaming records from a Kinesis stream
+    * On stream finish (due to error or other), worker will be shutdown
     *
     *  @tparam F effect type of the fs2 stream
     *  @param workerFactory function to create a Worker from a IRecordProcessorFactory
@@ -25,24 +26,33 @@ package object kcl {
   )(implicit F: ConcurrentEffect[F]): fs2.Stream[F, CommittableRecord] = {
 
     // Initialize a KCL worker which appends to the internal stream queue on message receipt
-    def instantiateWorker(queue: Queue[F, CommittableRecord]) = F.delay {
+    def instantiateWorker(queue: Queue[F, CommittableRecord]): F[Worker] = F.delay {
       workerFactory(
         new IRecordProcessorFactory {
-          override def createProcessor() = {
+          override def createProcessor() =
             new RecordProcessor(record => F.runAsync(queue.enqueue1(record))(_ => IO.unit).unsafeRunSync)
-          }
         }
-      ).run
+      )
     }
 
-    // Instantiate a new bounded queue and concurrently run the queue populator and expose the elements
-    // via a dequeuer
-    Stream.eval(Queue.bounded[F, CommittableRecord](config.bufferSize)) flatMap { buffer =>
-      val worker = Stream.eval(instantiateWorker(buffer)).drain
-      buffer.dequeue concurrently worker
-    }
+    // Instantiate a new bounded queue and concurrently run the queue populator
+    // Expose the elements by dequeuing the internal buffer
+    for {
+      buffer <- Stream.eval(Queue.bounded[F, CommittableRecord](config.bufferSize))
+      worker = instantiateWorker(buffer)
+      stream <- buffer.dequeue concurrently Stream.eval(worker.map(_.run)) onFinalize(worker.map(_.shutdown))
+    } yield stream
   }
 
+  /** Pipe to checkpoint records in Kinesis, marking them as processed
+    * Groups records by shard id, so that each shard is subject to its own clustering of records
+    * After accumulating maxBatchSize or reaching maxBatchWait for a respective shard, the latest record is checkpointed
+    * By design, all records prior to the checkpointed record are also checkpointed in Kinesis
+    *
+    *  @tparam F effect type of the fs2 stream
+    *  @param checkpointSettings configure maxBatchSize and maxBatchWait time before triggering a checkpoint
+    *  @return a stream of Record types representing checkpointed messages
+    */
   def checkpointRecords[F[_]](
     checkpointSettings: KinesisWorkerCheckpointSettings = KinesisWorkerCheckpointSettings.defaultInstance
   )(implicit F: ConcurrentEffect[F], timer: Timer[F]): fs2.Pipe[F, CommittableRecord, Record] = {
@@ -53,16 +63,24 @@ package object kcl {
           .map(_.toList.max)
           .mapAsync(1) { cr =>
             F.async[Record](_ =>
-              if (cr._2.canCheckpoint) {
+              if (cr._2.canCheckpoint)
                 Right(cr._2.checkpoint)
-              }
               else
-                Left(throw new Exception("record processor shutdown"))
+                Left(throw new Exception("Record processor has been shutdown - cannot checkpoint records"))
             )
           }
       }.parJoinUnbounded
   }
 
+  /** Sink to checkpoint records in Kinesis, marking them as processed
+    * Groups records by shard id, so that each shard is subject to its own clustering of records
+    * After accumulating maxBatchSize or reaching maxBatchWait for a respective shard, the latest record is checkpointed
+    * By design, all records prior to the checkpointed record are also checkpointed in Kinesis
+    *
+    *  @tparam F effect type of the fs2 stream
+    *  @param checkpointSettings configure maxBatchSize and maxBatchWait time before triggering a checkpoint
+    *  @return a Sink that accepts a stream of CommittableRecords
+    */
   def checkpointRecords_[F[_]](
     checkpointSettings: KinesisWorkerCheckpointSettings = KinesisWorkerCheckpointSettings.defaultInstance
   )(implicit F: ConcurrentEffect[F], timer: Timer[F]): fs2.Sink[F, CommittableRecord] = {
--- src/test/scala/KinesisConsumerSpec.scala
+++ src/test/scala/KinesisConsumerSpec.scala
@@ -22,6 +22,7 @@ import org.mockito.Mockito._
 
 import java.util.Date
 import java.nio.ByteBuffer
+import scala.concurrent.Future
 import scala.collection.JavaConverters._
 
 class KinesisConsumerSpec extends FlatSpec with Matchers with BeforeAndAfterEach with Eventually {
@@ -30,14 +31,99 @@ class KinesisConsumerSpec extends FlatSpec with Matchers with BeforeAndAfterEach
   implicit val timer: Timer[IO] = IO.timer(ec)
   implicit val ioContextShift: ContextShift[IO] = IO.contextShift(ec)
 
-  "Reading data from a Kinesis stream via readFromStream" should "successfully read data from the Kinesis stream" in new WorkerContext with TestData {
+  "KinesisWorker source" should "successfully read data from the Kinesis stream" in new WorkerContext with TestData {
     recordProcessor.initialize(initializationInput)
     recordProcessor.processRecords(recordsInput)
 
+    eventually(verify(mockWorker, times(1)).run())
+
     eventually(timeout(1.second)) {
-      output.head.record.getData should be(record.getData)
+      val commitableRecord = output.head
+      commitableRecord.record.getData should be(record.getData)
+      commitableRecord.recordProcessorStartingSequenceNumber shouldBe initializationInput.getExtendedSequenceNumber
+      commitableRecord.shardId shouldBe initializationInput.getShardId
+      commitableRecord.millisBehindLatest shouldBe recordsInput.getMillisBehindLatest
     }
   }
+
+  it should "not shutdown the worker if the stream is drained but has not failed" in new WorkerContext with TestData {
+    recordProcessor.initialize(initializationInput)
+    recordProcessor.processRecords(recordsInput)
+
+    eventually(verify(mockWorker, times(0)).shutdown())
+  }
+
+  it should "shutdown the worker if the stream terminates" in new WorkerContext(errorStream = true) with TestData {
+    recordProcessor.initialize(initializationInput)
+    recordProcessor.processRecords(recordsInput)
+
+    eventually(verify(mockWorker, times(1)).shutdown())
+  }
+
+  it should "not drop messages in case of back-pressure" in new WorkerContext with TestData {
+    // Create and send 10 records (to match buffer size)
+    for(i <- 1 to 10) {
+      val record = mock(classOf[Record])
+      when(record.getSequenceNumber).thenReturn(i.toString)
+      recordProcessor.processRecords(
+        recordsInput.withRecords(List(record).asJava))
+    }
+
+    // Should process all 10 messages
+    eventually(output.size shouldBe(10))
+
+    // Send a batch that exceeds the internal buffer size
+    for(i <- 1 to 50) {
+      val record = mock(classOf[Record])
+      when(record.getSequenceNumber).thenReturn(i.toString)
+      recordProcessor.processRecords(
+        recordsInput.withRecords(List(record).asJava))
+    }
+
+    // Should process all 50 messages
+    eventually(output.size shouldBe(60))
+
+    eventually(verify(mockWorker, times(0)).shutdown())
+  }
+
+  it should "not drop messages in case of back-pressure with multiple shard workers" in new WorkerContext with TestData {
+    recordProcessor.initialize(initializationInput)
+    recordProcessor2.initialize(initializationInput.withShardId("shard2"))
+
+    // Create and send 10 records (to match buffer size)
+    for(i <- 1 to 5) {
+      val record = mock(classOf[Record])
+      when(record.getSequenceNumber).thenReturn(i.toString)
+      recordProcessor.processRecords(
+        recordsInput.withRecords(List(record).asJava))
+      recordProcessor2.processRecords(
+        recordsInput.withRecords(List(record).asJava))
+    }
+
+    // Should process all 10 messages
+    eventually(output.size shouldBe(10))
+
+    // Each shard is assigned its own worker thread, so we get messages
+    // from each thread simultaneously.
+    def simulateWorkerThread(rp: IRecordProcessor): Future[Unit] = {
+      Future {
+        for (i <- 1 to 25) { // 10 is a buffer size
+          val record = mock(classOf[Record])
+          when(record.getSequenceNumber).thenReturn(i.toString)
+          rp.processRecords(recordsInput.withRecords(List(record).asJava))
+        }
+      }
+    }
+
+    simulateWorkerThread(recordProcessor)
+    simulateWorkerThread(recordProcessor2)
+
+    // Should process all 50 messages
+    eventually(output.size shouldBe(60))
+
+    eventually(verify(mockWorker, times(0)).shutdown())
+  }
+
 
   "KinesisWorker checkpoint pipe" should "checkpoint batch of records with same sequence number" in new KinesisWorkerCheckpointContext {
     val input = (1 to 3) map { i =>
@@ -46,7 +132,7 @@ class KinesisConsumerSpec extends FlatSpec with Matchers with BeforeAndAfterEach
       when(record.getSubSequenceNumber).thenReturn(i.toLong)
       new CommittableRecord(
         "shard-1",
-        org.mockito.Mockito.mock(classOf[ExtendedSequenceNumber]),
+        mock(classOf[ExtendedSequenceNumber]),
         1L,
         record,
         recordProcessor,
@@ -127,7 +213,7 @@ class KinesisConsumerSpec extends FlatSpec with Matchers with BeforeAndAfterEach
 
     val input = new CommittableRecord(
       "shard-1",
-      org.mockito.Mockito.mock(classOf[ExtendedSequenceNumber]),
+      mock(classOf[ExtendedSequenceNumber]),
       1L,
       record,
       recordProcessor,
@@ -148,29 +234,31 @@ class KinesisConsumerSpec extends FlatSpec with Matchers with BeforeAndAfterEach
   }
 
 
-  private abstract class WorkerContext(backpressureTimeout: FiniteDuration = 1.minute) {
+  private abstract class WorkerContext(backpressureTimeout: FiniteDuration = 1.minute, errorStream: Boolean = false) {
 
     var output: List[CommittableRecord] = List()
 
     protected val mockWorker = mock(classOf[Worker])
 
-    when(mockWorker.run()).thenAnswer(
-      new Answer[Unit] {
-        override def answer(invocation: InvocationOnMock): Unit = ()
-      })
+    when(mockWorker.run()).thenAnswer(new Answer[Unit] { override def answer(invocation: InvocationOnMock): Unit = () })
 
     var recordProcessorFactory: IRecordProcessorFactory = _
     var recordProcessor: IRecordProcessor = _
+    var recordProcessor2: IRecordProcessor = _
 
     val builder = { x: IRecordProcessorFactory =>
       recordProcessorFactory = x
       recordProcessor = x.createProcessor()
+      recordProcessor2 = x.createProcessor()
       mockWorker
     }
 
+    val config = KinesisWorkerStreamSettings(bufferSize = 10, backpressureTimeout = 1.second)
+
     val stream =
-      readFromKinesisStream[IO](builder)
+      readFromKinesisStream[IO](builder, config)
         .through(_.evalMap(i => IO(output = output :+ i)))
+        .map(i => if(errorStream) throw new Exception("boom") else i)
         .compile
         .toVector
         .unsafeRunAsync(_ => ())
