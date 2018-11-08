51ce53ee035d358a47abae2e442cf84ad27d5d78
Dmytro Semenov <dsemenov@gilt.com>
2018-11-08 10:12:27 -0500

    bypass original payload for kinesis publisher

--- src/test/scala/KinesisProducerSpec.scala
+++ src/test/scala/KinesisProducerSpec.scala
@@ -20,10 +20,10 @@ class KinesisProducerSpec extends FlatSpec with Matchers with BeforeAndAfterEach
     implicit val ioContextShift: ContextShift[IO] = IO.contextShift(ec)
     implicit val timer: Timer[IO]                 = IO.timer(ec)
     import fs2.aws.internal.Internal._
-
+    val successResult = new UserRecordResult(List[Attempt]().asJava, "seq #", "shard #", true)
     val ops = IO {
       val future: SettableFuture[UserRecordResult] = SettableFuture.create()
-      future.set(new UserRecordResult(List[Attempt]().asJava, "seq #", "shard #", true))
+      future.set(successResult)
       future
     }
 
@@ -64,6 +64,21 @@ class KinesisProducerSpec extends FlatSpec with Matchers with BeforeAndAfterEach
 
     KinesisStub._data.size should be(1)
     KinesisStub._data.head should be(ByteBuffer.wrap("someData".getBytes))
+  }
+
+  "Publishing data to a Kinesis stream via writeToKinesis1_" should "successfully save data to the stream bypassing original payload" in new KinesisProducerTestContext {
+    implicit def encoder(in: String): ByteBuffer = ByteBuffer.wrap(in.getBytes)
+    val result = fs2.Stream
+      .eval(IO.pure("someData"))
+      .flatMap(i => fs2.Stream.emit(("partitionKey", i)))
+      .through(writeToKinesis1[IO, String]("test-stream", kinesisProducerTestClient))
+      .compile
+      .toVector
+      .unsafeRunSync
+
+    KinesisStub._data.size should be(1)
+    KinesisStub._data.head should be(ByteBuffer.wrap("someData".getBytes))
+    result should be(Vector("someData" -> successResult))
   }
 
   "error thrown when invoking writeToKinesis" should "return an error in the stream" in new KinesisProducerTestContext {
