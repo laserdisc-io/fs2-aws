61f711291ac567adf3c3b61de7f3601baf161eca
Christopher Poenaru <kiambogo@gmail.com>
2018-12-25 20:35:51 -0500

    SQS Consumer with AWS SDK v2

---
+++ fs2-aws/src/main/scala/fs2/aws/sqs/consumer.scala
@@ -0,0 +1,72 @@
+package fs2.aws.sqs
+
+import fs2.{Stream, Pipe, Sink}
+import fs2.concurrent.Queue
+import cats.effect.{Async, ConcurrentEffect}
+import software.amazon.awssdk.services.sqs.SqsAsyncClient
+import software.amazon.awssdk.services.sqs.model._
+
+import scala.util.{Success, Failure}
+import scala.compat.java8.FutureConverters._
+import scala.concurrent.ExecutionContext
+import scala.collection.JavaConverters._
+
+object consumer {
+  def readObjectFromSqs[F[_], A](queueUrl: String)(
+      implicit ec: ExecutionContext,
+      F: ConcurrentEffect[F],
+      decoder: Message => Either[Throwable, A]): Stream[F, Either[Throwable, A]] = {
+    val client = SqsAsyncClient.create()
+
+    def receiveMessageStream =
+      Stream
+        .eval(
+          F.async[List[Message]] { cb =>
+            client.receiveMessage(buildReceiveRequest(queueUrl, 5)).toScala.onComplete {
+              case Success(response) => cb(Right(response.messages().asScala.toList))
+              case Failure(err)      => cb(Left(err))
+            }
+          }
+        )
+        .flatMap(list => Stream.emits(list.map(decoder)))
+
+    for {
+      buffer <- Stream.eval(Queue.bounded[F, Either[Throwable, A]](10))
+      stream <- buffer.dequeue concurrently receiveMessageStream
+    } yield stream
+  }
+
+  def readFromSqs[F[_]](queueUrl: String)(implicit ec: ExecutionContext,
+                                          conEff: ConcurrentEffect[F]): Stream[F, Message] =
+    readObjectFromSqs[F, Message](queueUrl)(ec, conEff, msg => Right(msg)).map(_.right.get)
+
+  def deleteFromSqs[F[_]](queueUrl: String)(
+      implicit ec: ExecutionContext,
+      F: Async[F]): Pipe[F, Message, DeleteMessageResponse] = {
+    val client = SqsAsyncClient.create()
+
+    _.flatMap { msg =>
+      Stream.eval(
+        F.async[DeleteMessageResponse] { cb =>
+          client.deleteMessage(buildDeleteRequest(queueUrl, msg)).toScala.onComplete {
+            case Success(response) => cb(Right(response))
+            case Failure(err)      => cb(Left(err))
+          }
+        }
+      )
+    }
+
+  }
+
+  def deleteFromSqs_[F[_]](queueUrl: String)(implicit ec: ExecutionContext,
+                                             F: Async[F]): Sink[F, Message] =
+    _.through(deleteFromSqs(queueUrl)).drain
+
+  private[this] def buildReceiveRequest(url: String, numMessages: Int): ReceiveMessageRequest = {
+    ReceiveMessageRequest.builder().queueUrl(url).maxNumberOfMessages(numMessages).build()
+  }
+
+  private[this] def buildDeleteRequest(url: String, message: Message): DeleteMessageRequest = {
+    DeleteMessageRequest.builder().queueUrl(url).receiptHandle(message.receiptHandle()).build()
+  }
+}
