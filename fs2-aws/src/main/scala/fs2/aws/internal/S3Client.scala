c169824434ac8f185fd316513d62806913140d04
Sean Sullivan <github@seansullivan.com>
2019-01-13 10:08:29 -0800

    tweak [S3Client.scala]

--- fs2-aws/src/main/scala/fs2/aws/internal/S3Client.scala
+++ fs2-aws/src/main/scala/fs2/aws/internal/S3Client.scala
@@ -12,12 +12,12 @@ import scala.util.control.Exception
 private[aws] trait S3Client[F[_]] {
   private lazy val client = S3AsyncClient.create
 
-  def getObjectContentOrError(getObjectRequest: GetObjectRequest)(
+  def getObjectContentOrError(request: GetObjectRequest)(
       implicit F: Effect[F]): F[Either[Throwable, InputStream]] =
-    F.delay(Exception.nonFatalCatch either client.getObject(getObjectRequest).getObjectContent)
+    F.delay(Exception.nonFatalCatch either client.getObject(request).getObjectContent)
 
-  def getObjectContent(getObjectRequest: GetObjectRequest)(implicit F: Effect[F]): F[InputStream] =
-    F.delay(client.getObject(getObjectRequest).getObjectContent)
+  def getObjectContent(request: GetObjectRequest)(implicit F: Effect[F]): F[InputStream] =
+    F.delay(client.getObject(request).getObjectContent)
 
   def initiateMultipartUpload(request: CreateMultipartUploadRequest)(
       implicit F: Effect[F]): F[CreateMultipartUploadRequest] =
@@ -30,12 +30,12 @@ private[aws] trait S3Client[F[_]] {
       implicit F: Effect[F]): F[CompleteMultipartUploadResponse] =
     F.delay(client.completeMultipartUpload(completeMultipartUploadRequest))
 
-  def s3ObjectSummaries(listObjectsV2Request: ListObjectsV2Request)(
-      implicit F: Effect[F]): F[List[S3ObjectSummary]] =
-    F.delay(client.listObjectsV2(listObjectsV2Request).getObjectSummaries.asScala.toList)
+  def s3ObjectSummaries(request: ListObjectsV2Request)(
+      implicit F: Effect[F]): F[List[S3Object]] =
+    F.delay(client.listObjectsV2(request).thenApply(_.contents))
 
-  def getObject(objectRequest: GetObjectRequest)(implicit F: Effect[F]): F[S3Object] = {
-    F.delay(client.getObject(objectRequest))
+  def getObject(request: GetObjectRequest)(implicit F: Effect[F]): F[S3Object] = {
+    F.delay(client.getObject(request))
   }
 
 }
