77deb86a32ab6d142141bcfc1c53e73ff0573fb2
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-08-25 17:18:23 +0200

    Update mockito-scala-scalatest to 1.15.0 (#451)
    Co-authored-by: semenodm <sdo.semenov@gmail.com>

--- project/V.scala
+++ project/V.scala
@@ -7,6 +7,6 @@ object V {
   val Refined          = "0.9.15"
   val Munit            = "0.7.11"
   val ScalaTest        = "3.2.1"
+  val MockitoScalaTest = "1.15.0"
   val MockitoCore      = "3.5.5"
-  val MockitoScalaTest = "1.14.8"
 }
