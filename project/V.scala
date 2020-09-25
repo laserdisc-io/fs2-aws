2ca36ed565ddfbe862f1f4a2fe819aa88cb780b1
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-09-25 16:20:53 +0200

    Update refined to 0.9.16 (#474)
    Co-authored-by: semenodm <sdo.semenov@gmail.com>

--- project/V.scala
+++ project/V.scala
@@ -4,7 +4,7 @@ object V {
   val Circe            = "0.13.0"
   val Ciris            = "0.12.1"
   val Fs2              = "2.4.4"
-  val Refined          = "0.9.15"
+  val Refined          = "0.9.16"
   val Munit            = "0.7.12"
   val ScalaTest        = "3.2.2"
   val MockitoScalaTest = "1.15.0"
