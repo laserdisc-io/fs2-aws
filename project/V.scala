27e8d8abf859da9be267a731177d1e888bc08673
Scala Steward <me@scala-steward.org>
2020-10-12 12:41:16 +0200

    Update munit to 0.7.14

--- project/V.scala
+++ project/V.scala
@@ -5,7 +5,7 @@ object V {
   val Ciris            = "0.12.1"
   val Fs2              = "2.4.4"
   val Refined          = "0.9.17"
-  val Munit            = "0.7.13"
+  val Munit            = "0.7.14"
   val ScalaTest        = "3.2.2"
   val MockitoScalaTest = "1.15.1"
   val MockitoCore      = "3.5.13"
