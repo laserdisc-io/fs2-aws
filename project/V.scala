6e50e2f8d488cd6d98d7461d157de95a7c6d65f5
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-11-13 02:25:31 +0100

    Update mockito-core to 3.5.15 (#500)
    Co-authored-by: semenodm <sdo.semenov@gmail.com>

--- project/V.scala
+++ project/V.scala
@@ -8,5 +8,5 @@ object V {
   val Munit            = "0.7.17"
   val ScalaTest        = "3.2.3"
   val MockitoScalaTest = "1.15.1"
-  val MockitoCore      = "3.5.13"
+  val MockitoCore      = "3.5.15"
 }
