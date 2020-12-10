2b46736019c4f0bfd9b741a2893c2e0e695049cd
Scala Steward <me@scala-steward.org>
2020-12-11 00:45:42 +0100

    Update mockito-scala-scalatest to 1.16.3

--- project/V.scala
+++ project/V.scala
@@ -7,6 +7,6 @@ object V {
   val Fs2              = "2.4.6"
   val Refined          = "0.9.19"
   val ScalaTest        = "3.2.3"
-  val MockitoScalaTest = "1.15.1"
+  val MockitoScalaTest = "1.16.3"
   val MockitoCore      = "3.6.28"
 }
