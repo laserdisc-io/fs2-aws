ae1618da531ce9293b21cbc1be408f0db7cd93c3
Scala Steward <me@scala-steward.org>
2020-12-15 06:32:39 +0100

    Update aws-java-sdk-kinesis, ... to 1.11.919

--- project/V.scala
+++ project/V.scala
@@ -1,5 +1,5 @@
 object V {
-  val AwsSdk           = "1.11.914"
+  val AwsSdk           = "1.11.919"
   val AwsSdkS3         = "2.15.35"
   val Circe            = "0.13.0"
   val Ciris            = "0.12.1"
