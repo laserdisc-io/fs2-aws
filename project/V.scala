f1da9625230deee9b508924f4dfab2b0fa3465db
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-02-24 19:31:02 +0100

    Update munit to 0.7.22 (#599)
    * Update munit to 0.7.22
    
    * Update munit to 0.7.22
    
    Co-authored-by: semenodm <sdo.semenov@gmail.com>

--- project/V.scala
+++ project/V.scala
@@ -2,7 +2,7 @@ object V {
   val AwsSdk           = "2.16.4"
   val Circe            = "0.13.0"
   val Ciris            = "0.12.1"
-  val Munit            = "0.7.21"
+  val Munit            = "0.7.22"
   val Fs2              = "2.5.3"
   val Refined          = "0.9.21"
   val ScalaTest        = "3.2.5"
