3b86520de6636b2fba1d4e7f2de667006424ce44
Scala Steward <me@scala-steward.org>
2021-02-24 04:31:57 +0100

    Update refined to 0.9.21

--- project/V.scala
+++ project/V.scala
@@ -4,7 +4,7 @@ object V {
   val Ciris            = "0.12.1"
   val Munit            = "0.7.21"
   val Fs2              = "2.5.3"
-  val Refined          = "0.9.20"
+  val Refined          = "0.9.21"
   val ScalaTest        = "3.2.3"
   val MockitoScalaTest = "1.16.25"
   val MockitoCore      = "3.7.7"
