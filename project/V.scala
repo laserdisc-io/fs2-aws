128d08d2b6aaf905d567ca6f8c4c9e64d56b1cab
Scala Steward <me@scala-steward.org>
2021-06-09 22:32:33 +0200

    Update mockito-core to 3.11.0

--- project/V.scala
+++ project/V.scala
@@ -7,6 +7,6 @@ object V {
   val Refined          = "0.9.25"
   val ScalaTest        = "3.2.8"
   val MockitoScalaTest = "1.16.37"
-  val MockitoCore      = "3.9.0"
+  val MockitoCore      = "3.11.0"
   val CE               = "3.1.0"
 }
