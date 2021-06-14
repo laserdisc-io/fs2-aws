b1c7a488f513396853994fa1e028236d8c6957c7
Scala Steward <me@scala-steward.org>
2021-06-14 19:07:08 +0200

    Update scalatest to 3.2.9

--- project/V.scala
+++ project/V.scala
@@ -5,7 +5,7 @@ object V {
   val Munit            = "0.7.25"
   val Fs2              = "3.0.4"
   val Refined          = "0.9.26"
-  val ScalaTest        = "3.2.8"
+  val ScalaTest        = "3.2.9"
   val MockitoScalaTest = "1.16.37"
   val MockitoCore      = "3.9.0"
   val CE               = "3.1.0"
