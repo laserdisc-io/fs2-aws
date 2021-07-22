3c102824c5c4e60ee23ef06367416d2985f2aade
Scala Steward <me@scala-steward.org>
2021-07-22 17:19:12 +0200

    Update munit to 0.7.27

--- project/V.scala
+++ project/V.scala
@@ -2,7 +2,7 @@ object V {
   val AwsSdk           = "2.16.93"
   val Circe            = "0.14.1"
   val Ciris            = "0.12.1"
-  val Munit            = "0.7.26"
+  val Munit            = "0.7.27"
   val Fs2              = "3.0.6"
   val Refined          = "0.9.27"
   val ScalaTest        = "3.2.9"
