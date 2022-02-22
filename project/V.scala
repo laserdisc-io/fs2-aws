954d0ac7844a66482c8d3043f962ea89a370db71
Scala Steward <me@scala-steward.org>
2022-02-22 10:29:19 +0100

    Update fs2-core, fs2-io to 3.2.5

--- project/V.scala
+++ project/V.scala
@@ -3,7 +3,7 @@ object V {
   val Circe            = "0.14.1"
   val Ciris            = "0.12.1"
   val Munit            = "0.7.29"
-  val Fs2              = "3.2.4"
+  val Fs2              = "3.2.5"
   val Refined          = "0.9.28"
   val ScalaTest        = "3.2.10"
   val MockitoScalaTest = "1.16.55"
