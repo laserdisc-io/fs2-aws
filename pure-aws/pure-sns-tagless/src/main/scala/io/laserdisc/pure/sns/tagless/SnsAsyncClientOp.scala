d7873bec708342e23165748f9036acf20f6a1aa3
Scala Steward <me@scala-steward.org>
2022-04-20 06:28:45 +0200

    Reformat with scalafmt 3.5.2

--- pure-aws/pure-sns-tagless/src/main/scala/io/laserdisc/pure/sns/tagless/SnsAsyncClientOp.scala
+++ pure-aws/pure-sns-tagless/src/main/scala/io/laserdisc/pure/sns/tagless/SnsAsyncClientOp.scala
@@ -6,51 +6,83 @@ import software.amazon.awssdk.services.sns.paginators.*
 trait SnsAsyncClientOp[F[_]] {
   // SnsAsyncClient
   def addPermission(a: AddPermissionRequest): F[AddPermissionResponse]
-  def checkIfPhoneNumberIsOptedOut(a: CheckIfPhoneNumberIsOptedOutRequest): F[CheckIfPhoneNumberIsOptedOutResponse]
+  def checkIfPhoneNumberIsOptedOut(
+      a: CheckIfPhoneNumberIsOptedOutRequest
+  ): F[CheckIfPhoneNumberIsOptedOutResponse]
   def close: F[Unit]
   def confirmSubscription(a: ConfirmSubscriptionRequest): F[ConfirmSubscriptionResponse]
-  def createPlatformApplication(a: CreatePlatformApplicationRequest): F[CreatePlatformApplicationResponse]
+  def createPlatformApplication(
+      a: CreatePlatformApplicationRequest
+  ): F[CreatePlatformApplicationResponse]
   def createPlatformEndpoint(a: CreatePlatformEndpointRequest): F[CreatePlatformEndpointResponse]
-  def createSMSSandboxPhoneNumber(a: CreateSmsSandboxPhoneNumberRequest): F[CreateSmsSandboxPhoneNumberResponse]
+  def createSMSSandboxPhoneNumber(
+      a: CreateSmsSandboxPhoneNumberRequest
+  ): F[CreateSmsSandboxPhoneNumberResponse]
   def createTopic(a: CreateTopicRequest): F[CreateTopicResponse]
   def deleteEndpoint(a: DeleteEndpointRequest): F[DeleteEndpointResponse]
-  def deletePlatformApplication(a: DeletePlatformApplicationRequest): F[DeletePlatformApplicationResponse]
-  def deleteSMSSandboxPhoneNumber(a: DeleteSmsSandboxPhoneNumberRequest): F[DeleteSmsSandboxPhoneNumberResponse]
+  def deletePlatformApplication(
+      a: DeletePlatformApplicationRequest
+  ): F[DeletePlatformApplicationResponse]
+  def deleteSMSSandboxPhoneNumber(
+      a: DeleteSmsSandboxPhoneNumberRequest
+  ): F[DeleteSmsSandboxPhoneNumberResponse]
   def deleteTopic(a: DeleteTopicRequest): F[DeleteTopicResponse]
   def getEndpointAttributes(a: GetEndpointAttributesRequest): F[GetEndpointAttributesResponse]
 
   def getPlatformApplicationAttributes(
-    a: GetPlatformApplicationAttributesRequest
+      a: GetPlatformApplicationAttributesRequest
   ): F[GetPlatformApplicationAttributesResponse]
   def getSMSAttributes: F[GetSmsAttributesResponse]
   def getSMSAttributes(a: GetSmsAttributesRequest): F[GetSmsAttributesResponse]
-  def getSMSSandboxAccountStatus(a: GetSmsSandboxAccountStatusRequest): F[GetSmsSandboxAccountStatusResponse]
-  def getSubscriptionAttributes(a: GetSubscriptionAttributesRequest): F[GetSubscriptionAttributesResponse]
+  def getSMSSandboxAccountStatus(
+      a: GetSmsSandboxAccountStatusRequest
+  ): F[GetSmsSandboxAccountStatusResponse]
+  def getSubscriptionAttributes(
+      a: GetSubscriptionAttributesRequest
+  ): F[GetSubscriptionAttributesResponse]
   def getTopicAttributes(a: GetTopicAttributesRequest): F[GetTopicAttributesResponse]
 
   def listEndpointsByPlatformApplication(
-    a: ListEndpointsByPlatformApplicationRequest
+      a: ListEndpointsByPlatformApplicationRequest
   ): F[ListEndpointsByPlatformApplicationResponse]
 
   def listEndpointsByPlatformApplicationPaginator(
-    a: ListEndpointsByPlatformApplicationRequest
+      a: ListEndpointsByPlatformApplicationRequest
   ): F[ListEndpointsByPlatformApplicationPublisher]
   def listOriginationNumbers(a: ListOriginationNumbersRequest): F[ListOriginationNumbersResponse]
-  def listOriginationNumbersPaginator(a: ListOriginationNumbersRequest): F[ListOriginationNumbersPublisher]
+  def listOriginationNumbersPaginator(
+      a: ListOriginationNumbersRequest
+  ): F[ListOriginationNumbersPublisher]
   def listPhoneNumbersOptedOut: F[ListPhoneNumbersOptedOutResponse]
-  def listPhoneNumbersOptedOut(a: ListPhoneNumbersOptedOutRequest): F[ListPhoneNumbersOptedOutResponse]
+  def listPhoneNumbersOptedOut(
+      a: ListPhoneNumbersOptedOutRequest
+  ): F[ListPhoneNumbersOptedOutResponse]
   def listPhoneNumbersOptedOutPaginator: F[ListPhoneNumbersOptedOutPublisher]
-  def listPhoneNumbersOptedOutPaginator(a: ListPhoneNumbersOptedOutRequest): F[ListPhoneNumbersOptedOutPublisher]
+  def listPhoneNumbersOptedOutPaginator(
+      a: ListPhoneNumbersOptedOutRequest
+  ): F[ListPhoneNumbersOptedOutPublisher]
   def listPlatformApplications: F[ListPlatformApplicationsResponse]
-  def listPlatformApplications(a: ListPlatformApplicationsRequest): F[ListPlatformApplicationsResponse]
+  def listPlatformApplications(
+      a: ListPlatformApplicationsRequest
+  ): F[ListPlatformApplicationsResponse]
   def listPlatformApplicationsPaginator: F[ListPlatformApplicationsPublisher]
-  def listPlatformApplicationsPaginator(a: ListPlatformApplicationsRequest): F[ListPlatformApplicationsPublisher]
-  def listSMSSandboxPhoneNumbers(a: ListSmsSandboxPhoneNumbersRequest): F[ListSmsSandboxPhoneNumbersResponse]
-  def listSMSSandboxPhoneNumbersPaginator(a: ListSmsSandboxPhoneNumbersRequest): F[ListSMSSandboxPhoneNumbersPublisher]
+  def listPlatformApplicationsPaginator(
+      a: ListPlatformApplicationsRequest
+  ): F[ListPlatformApplicationsPublisher]
+  def listSMSSandboxPhoneNumbers(
+      a: ListSmsSandboxPhoneNumbersRequest
+  ): F[ListSmsSandboxPhoneNumbersResponse]
+  def listSMSSandboxPhoneNumbersPaginator(
+      a: ListSmsSandboxPhoneNumbersRequest
+  ): F[ListSMSSandboxPhoneNumbersPublisher]
   def listSubscriptions: F[ListSubscriptionsResponse]
   def listSubscriptions(a: ListSubscriptionsRequest): F[ListSubscriptionsResponse]
-  def listSubscriptionsByTopic(a: ListSubscriptionsByTopicRequest): F[ListSubscriptionsByTopicResponse]
-  def listSubscriptionsByTopicPaginator(a: ListSubscriptionsByTopicRequest): F[ListSubscriptionsByTopicPublisher]
+  def listSubscriptionsByTopic(
+      a: ListSubscriptionsByTopicRequest
+  ): F[ListSubscriptionsByTopicResponse]
+  def listSubscriptionsByTopicPaginator(
+      a: ListSubscriptionsByTopicRequest
+  ): F[ListSubscriptionsByTopicPublisher]
   def listSubscriptionsPaginator: F[ListSubscriptionsPublisher]
   def listSubscriptionsPaginator(a: ListSubscriptionsRequest): F[ListSubscriptionsPublisher]
   def listTagsForResource(a: ListTagsForResourceRequest): F[ListTagsForResourceResponse]
@@ -66,15 +98,19 @@ trait SnsAsyncClientOp[F[_]] {
   def setEndpointAttributes(a: SetEndpointAttributesRequest): F[SetEndpointAttributesResponse]
 
   def setPlatformApplicationAttributes(
-    a: SetPlatformApplicationAttributesRequest
+      a: SetPlatformApplicationAttributesRequest
   ): F[SetPlatformApplicationAttributesResponse]
   def setSMSAttributes(a: SetSmsAttributesRequest): F[SetSmsAttributesResponse]
-  def setSubscriptionAttributes(a: SetSubscriptionAttributesRequest): F[SetSubscriptionAttributesResponse]
+  def setSubscriptionAttributes(
+      a: SetSubscriptionAttributesRequest
+  ): F[SetSubscriptionAttributesResponse]
   def setTopicAttributes(a: SetTopicAttributesRequest): F[SetTopicAttributesResponse]
   def subscribe(a: SubscribeRequest): F[SubscribeResponse]
   def tagResource(a: TagResourceRequest): F[TagResourceResponse]
   def unsubscribe(a: UnsubscribeRequest): F[UnsubscribeResponse]
   def untagResource(a: UntagResourceRequest): F[UntagResourceResponse]
-  def verifySMSSandboxPhoneNumber(a: VerifySmsSandboxPhoneNumberRequest): F[VerifySmsSandboxPhoneNumberResponse]
+  def verifySMSSandboxPhoneNumber(
+      a: VerifySmsSandboxPhoneNumberRequest
+  ): F[VerifySmsSandboxPhoneNumberResponse]
 
 }
