406b91d20ad1b4c6c967a54a82aed9fd84d724ad
Scala Steward <scala_steward@virtuslab.com>
2022-07-06 17:53:26 +0000

    Update circe-core, circe-generic, ... to 0.14.2

--- project/V.scala
+++ project/V.scala
@@ -1,6 +1,6 @@
 object V {
   val AwsSdk           = "2.17.198"
-  val Circe            = "0.14.1"
+  val Circe            = "0.14.2"
   val Ciris            = "0.12.1"
   val Munit            = "0.7.29"
   val Fs2              = "3.2.7"
