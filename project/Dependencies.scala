5340fa749e4a2e432e615d4290075a2751b99552
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-02-03 00:39:14 +0100

    Update scalatest to 3.2.15 (#968)
    Co-authored-by: semenodm <sdo.semenov@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val Munit            = "0.7.29"
     val Fs2              = "3.5.0"
     val Refined          = "0.10.1"
-    val ScalaTest        = "3.2.14"
+    val ScalaTest        = "3.2.15"
     val MockitoScalaTest = "1.17.5"
     val MockitoCore      = "4.8.1"
     val CE               = "3.4.5"
