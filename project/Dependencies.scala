59fa81154fe198d43e728c4b6511d3f67f00c6c8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-02-03 17:36:40 +0100

    Update mockito-core to 4.11.0 (#963)
    * Update mockito-core to 4.11.0
    
    * Revert commit(s) 8268e2a
    
    * Update mockito-core to 4.11.0
    
    ---------
    
    Co-authored-by: semenodm <sdo.semenov@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val Refined          = "0.10.1"
     val ScalaTest        = "3.2.15"
     val MockitoScalaTest = "1.17.5"
-    val MockitoCore      = "4.8.1"
+    val MockitoCore      = "4.11.0"
     val CE               = "3.4.5"
     val Logback          = "1.4.4"
     val SLF4J            = "2.0.3"
