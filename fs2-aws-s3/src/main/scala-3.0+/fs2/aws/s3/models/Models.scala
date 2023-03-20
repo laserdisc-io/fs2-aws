b39e74f50498dcf16a63b474477f6887f5fb4484
Todd Burnside <tburnside@gemini.edu>
2023-03-20 13:11:02 -0700

    Change PartSizeMB to `GreaterEqual[5]` (#1002)

--- fs2-aws-s3/src/main/scala-3.0+/fs2/aws/s3/models/Models.scala
+++ fs2-aws-s3/src/main/scala-3.0+/fs2/aws/s3/models/Models.scala
@@ -1,16 +1,14 @@
 package fs2.aws.s3.models
 
 import eu.timepit.refined.api.{Refined, RefinedTypeOps}
-import eu.timepit.refined.boolean.Or
-import eu.timepit.refined.generic.Equal
-import eu.timepit.refined.numeric.Greater
+import eu.timepit.refined.numeric.GreaterEqual
 import eu.timepit.refined.types.string.NonEmptyString
 
 object Models {
 
   // Each part must be at least 5 MB in size
   // https://sdk.amazonaws.com/java/api/latest/software/amazon/awssdk/services/s3/S3Client.html#uploadPart-software.amazon.awssdk.services.s3.model.UploadPartRequest-software.amazon.awssdk.core.sync.RequestBody-
-  type PartSizeMB = Int Refined (Greater[5] Or Equal[5])
+  type PartSizeMB = Int Refined GreaterEqual[5]
   type ETag       = String
 
   object PartSizeMB extends RefinedTypeOps.Numeric[PartSizeMB, Int]
