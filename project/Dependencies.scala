3ec3aea3cdfb71f722d0a7c30647472f55d053cd
Scala Steward <scala_steward@virtuslab.com>
2023-05-25 00:14:23 +0000

    Update logback-classic, logback-core to 1.4.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     val MockitoScalaTest = "1.17.5"
     val MockitoCore      = "5.1.1"
     val CE               = "3.4.10"
-    val Logback          = "1.4.5"
+    val Logback          = "1.4.7"
     val SLF4J            = "2.0.6"
     val Log4Cats         = "2.5.0"
 
