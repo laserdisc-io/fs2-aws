0253009395f8b780059c02e936065f510a505dc8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-07-12 17:55:34 +0200

    Update cloudwatch, dynamodb, kinesis, sns, sqs to 2.20.100 (#1064)
    Co-authored-by: Dmytro Obodowsky <sdo.semenov@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt.{Def, _}
 object Dependencies {
 
   object V {
-    val AwsSdk           = "2.20.90"
+    val AwsSdk           = "2.20.100"
     val Circe            = "0.14.5"
     val Munit            = "0.7.29"
     val Fs2              = "3.7.0"
