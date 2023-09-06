8c281f838d4d1cb3213b28d11ddbb5759d15287a
Scala Steward <scala_steward@virtuslab.com>
2023-09-06 23:42:23 +0000

    Update jcl-over-slf4j, jul-to-slf4j to 2.0.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val MockitoCore      = "5.5.0"
     val CE               = "3.5.1"
     val Logback          = "1.4.11"
-    val SLF4J            = "2.0.7"
+    val SLF4J            = "2.0.9"
     val Log4Cats         = "2.6.0"
   }
 
