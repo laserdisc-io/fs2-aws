e2e5c5045dd8b74ad4f343681b766fa1102a7a17
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-06-24 19:56:40 +0200

    Update circe-core, circe-generic, ... to 0.14.8 (#1208)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
   object V {
     val AwsSdk           = "2.25.70"
-    val Circe            = "0.14.7"
+    val Circe            = "0.14.8"
     val Munit            = "1.0.0"
     val Fs2              = "3.10.2"
     val Refined          = "0.11.2"
