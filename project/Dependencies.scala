e9cbbd297679729db8bd998ab54b0d2fff08985f
Scala Steward <scala_steward@virtuslab.com>
2025-01-02 16:34:19 +0000

    Update cloudwatch, dynamodb, kinesis, s3, sns, ... to 2.29.43

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt.{Def, _}
 object Dependencies {
 
   object V {
-    val AwsSdk           = "2.29.40"
+    val AwsSdk           = "2.29.43"
     val Circe            = "0.14.9"
     val Munit            = "1.0.3"
     val Fs2              = "3.11.0"
