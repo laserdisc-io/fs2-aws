28018308332b2822cb8fa7ae1271af063fefe73e
Barry O'Neill <barra.oneill@gmail.com>
2025-01-13 10:48:46 -0500

    minor library upgrades

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,16 +5,16 @@ import sbt.{Def, _}
 object Dependencies {
 
   object V {
-    val AwsSdk           = "2.29.43"
-    val Circe            = "0.14.9"
-    val Munit            = "1.0.3"
+    val AwsSdk           = "2.29.50"
+    val Circe            = "0.14.10"
+    val Munit            = "1.0.4"
     val Fs2              = "3.11.0"
     val Refined          = "0.11.3"
     val ScalaTest        = "3.2.19"
     val MockitoScalaTest = "1.17.5"
-    val MockitoCore      = "5.14.2"
+    val MockitoCore      = "5.15.2"
     val CE               = "3.5.7"
-    val Logback          = "1.5.15"
+    val Logback          = "1.5.16"
     val SLF4J            = "2.0.16"
     val Log4Cats         = "2.7.0"
   }
@@ -26,7 +26,7 @@ object Dependencies {
 
   val CatsEffect = libraryDependencies += "org.typelevel" %% "cats-effect" % V.CE
 
-  val KinesisClient = libraryDependencies += "software.amazon.kinesis" % "amazon-kinesis-client" % "2.6.0"
+  val KinesisClient = libraryDependencies += "software.amazon.kinesis" % "amazon-kinesis-client" % "2.6.1"
 
   val KinesisProducer = libraryDependencies += "com.amazonaws" % "amazon-kinesis-producer" % "0.15.12"
 
