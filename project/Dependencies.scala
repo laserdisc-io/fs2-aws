1d91d0a4d280b7e0790468dfc7c773938e7c280e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-10-11 17:59:33 +0200

    Update cloudwatch, dynamodb, kinesis, s3, sns, ... to 2.35.5 (#1369)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt.{Def, _}
 object Dependencies {
 
   object V {
-    val AwsSdk           = "2.33.13"
+    val AwsSdk           = "2.35.5"
     val Circe            = "0.14.15"
     val Munit            = "1.2.0"
     val Fs2              = "3.12.2"
