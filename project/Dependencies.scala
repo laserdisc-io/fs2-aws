31965dc74424cde2fcc95b4ac5fc9095121079a4
Scala Steward <scala_steward@virtuslab.com>
2026-03-10 19:03:17 +0000

    Update munit to 1.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   object V {
     val AwsSdk           = "2.42.4"
     val Circe            = "0.14.15"
-    val Munit            = "1.2.3"
+    val Munit            = "1.2.4"
     val Fs2              = "3.12.2"
     val Refined          = "0.11.3"
     val ScalaTest        = "3.2.19"
