2d24e161382bd564e9a905f458c61b359eae5e87
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-07-24 18:50:05 +0200

    Update logback-classic, logback-core to 1.6.0 (#1488)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     val MockitoScalaTest = "1.17.5"
     val MockitoCore      = "5.23.0"
     val CE               = "3.7.0"
-    val Logback          = "1.5.38"
+    val Logback          = "1.6.0"
     val SLF4J            = "2.0.18"
     val Log4Cats         = "2.8.0"
   }
