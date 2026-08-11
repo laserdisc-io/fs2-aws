e0e9e18e0c4cece2aee5906e31437f64eaff4cd2
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-08-11 18:55:49 +0200

    Update munit to 1.3.5 (#1507)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   object V {
     val AwsSdk           = "2.50.2"
     val Circe            = "0.14.16"
-    val Munit            = "1.3.4"
+    val Munit            = "1.3.5"
     val Fs2              = "3.13.0"
     val Refined          = "0.11.4"
     val ScalaTest        = "3.2.20"
